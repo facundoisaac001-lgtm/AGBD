8e702686169541c3396213187d090260c25adda7
facundo <samuelhuallpa@etec.uba.ar>
2026-04-21 16:33:22 -0300

    laburo nuevo

--- facundo-torres-chinook.sql
+++ facundo-torres-chinook.sql
@@ -22,3 +22,44 @@ JOIN albums a ON t.AlbumId = a.AlbumId
 JOIN artists ar ON a.ArtistId = ar.ArtistId
 ORDER BY t.Milliseconds ASC
 LIMIT 20;
+
+SELECT FirstName, LastName
+FROM Employee
+ORDER BY LastName, FirstName;
+
+SELECT t.Name, t.Milliseconds
+FROM tracks t
+JOIN albums a ON t.AlbumId = a.AlbumId
+WHERE a.Title = 'Big Ones'
+ORDER BY t.Milliseconds DESC;
+
+SELECT name,UnitPrice FROM tracks ORDER by 
+UnitPrice  ASC LIMIT 10 
+
+SELECT t.Name, g.Name, a.Title
+FROM tracks t
+JOIN genres g ON t.GenreId = g.GenreId
+JOIN albums a ON t.AlbumId = a.AlbumId
+
+SELECT t.Name, t.Milliseconds, a.Title, ar.Name
+FROM tracks t
+JOIN albums a ON t.AlbumId = a.AlbumId
+JOIN artists ar ON a.ArtistId = ar.ArtistId
+ORDER BY t.Milliseconds ASC
+LIMIT 20;
+
+6.
+SELECT emp.LastName AS empleado, jefe.LastName AS jefe, COUNT(*)  FROM employees emp
+JOIN employees jefe ON emp.ReportsTo = jefe.EmployeeId
+JOIN customers cus ON emp.EmployeeId = cus.SupportRepId
+GROUP BY emp.EmployeeId
+ORDER by jefe ASC
+
+7.
+SELECT 
+e.FirstName AS NombreEmpleado,
+e.LastName AS ApellidoEmpleado,
+c.FirstName AS NombreCliente,
+c.LastName AS ApellidoCliente
+FROM employees e
+JOIN customers c ON e.EmployeeId = c.SupportRepId;
