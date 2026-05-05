3d498fa3bfd7de7eb83a12b93e8c9a340895159a
Lou-uw <lourdessoto@etec.uba.ar>
2026-05-05 16:25:24 -0300

    laburo total

--- facundo-torres-chinook.sql
+++ facundo-torres-chinook.sql
@@ -1,65 +1,85 @@
-SELECT FirstName, LastName
-FROM Employee
-ORDER BY LastName, FirstName;
+1.
+SELECT FirstName, LastName FROM employees 
+ORDER BY FirstName, LastName DESC 
 
-SELECT t.Name, t.Milliseconds
-FROM tracks t
-JOIN albums a ON t.AlbumId = a.AlbumId
-WHERE a.Title = 'Big Ones'
-ORDER BY t.Milliseconds DESC;
+2.
+SELECT name, Milliseconds FROM tracks
+ JOIN albums ON tracks.AlbumId = albums.AlbumId 
+ WHERE Title LIKE "Big Ones" ORDER BY Milliseconds DESC 
 
-SELECT name,UnitPrice FROM tracks ORDER by 
-UnitPrice  ASC LIMIT 10 
+3.
+SELECT Title, sum(UnitPrice) FROM tracks
+JOIN albums ON tracks.AlbumId = albums.AlbumId
+GROUP BY tracks.AlbumId
+ORDER BY sum(UnitPrice) ASC LIMIT 10
 
-SELECT t.Name, g.Name, a.Title
-FROM tracks t
-JOIN genres g ON t.GenreId = g.GenreId
-JOIN albums a ON t.AlbumId = a.AlbumId
+4.
+SELECT tracks.name, albums.Title,genres.Name FROM tracks
+JOIN albums ON tracks.AlbumId = albums.AlbumId
+JOIN genres ON tracks.GenreId = genres.GenreId
+WHERE UnitPrice = 0.99
 
-SELECT t.Name, t.Milliseconds, a.Title, ar.Name
-FROM tracks t
-JOIN albums a ON t.AlbumId = a.AlbumId
-JOIN artists ar ON a.ArtistId = ar.ArtistId
-ORDER BY t.Milliseconds ASC
-LIMIT 20;
-
-SELECT FirstName, LastName
-FROM Employee
-ORDER BY LastName, FirstName;
-
-SELECT t.Name, t.Milliseconds
-FROM tracks t
-JOIN albums a ON t.AlbumId = a.AlbumId
-WHERE a.Title = 'Big Ones'
-ORDER BY t.Milliseconds DESC;
-
-SELECT name,UnitPrice FROM tracks ORDER by 
-UnitPrice  ASC LIMIT 10 
-
-SELECT t.Name, g.Name, a.Title
-FROM tracks t
-JOIN genres g ON t.GenreId = g.GenreId
-JOIN albums a ON t.AlbumId = a.AlbumId
-
-SELECT t.Name, t.Milliseconds, a.Title, ar.Name
-FROM tracks t
-JOIN albums a ON t.AlbumId = a.AlbumId
-JOIN artists ar ON a.ArtistId = ar.ArtistId
-ORDER BY t.Milliseconds ASC
-LIMIT 20;
+5.
+SELECT tracks.name, tracks.Milliseconds, 
+albums.Title, artists.Name FROM tracks
+JOIN albums ON tracks.AlbumId = albums.AlbumId
+JOIN artists ON albums.ArtistId = artists.ArtistId
+WHERE UnitPrice = 0.99 
+ORDER BY tracks.Milliseconds ASC LIMIT 20
 
 6.
-SELECT emp.LastName AS empleado, jefe.LastName AS jefe, COUNT(*)  FROM employees emp
+SELECT emp.LastName AS empleado, jefe.LastName AS jefes, emp.Title, count(SupportRepId) 
+AS cant_clientes FROM employees emp
 JOIN employees jefe ON emp.ReportsTo = jefe.EmployeeId
-JOIN customers cus ON emp.EmployeeId = cus.SupportRepId
-GROUP BY emp.EmployeeId
-ORDER by jefe ASC
+JOIN customers ON emp.EmployeeId = customers.SupportRepId
+GROUP BY emp.EmployeeId ORDER BY cant_clientes DESC
 
 7.
-SELECT 
-e.FirstName AS NombreEmpleado,
-e.LastName AS ApellidoEmpleado,
-c.FirstName AS NombreCliente,
-c.LastName AS ApellidoCliente
-FROM employees e
-JOIN customers c ON e.EmployeeId = c.SupportRepId;
+SELECT emp.FirstName AS Nombre_empleado, emp.LastName AS Apellido_empleado,
+ clie.FirstName AS Nombre_cliente, clie.LastName AS Apellido_cliente
+FROM employees emp
+JOIN customers clie ON emp.EmployeeId = clie.SupportRepId
+
+8.
+SELECT clie.FirstName AS Nombre_cliente, clie.LastName AS Apellido_cliente, clie.Address,inv.InvoiceDate
+FROM customers clie
+JOIN invoices inv ON clie.CustomerId = inv.InvoiceId
+
+9.
+SELECT genres.name AS Genero, COUNT(tracks.TrackId) AS Total_canciones FROM genres 
+JOIN tracks  ON genres.GenreId = tracks.GenreId
+GROUP BY genres.name ORDER BY Total_canciones DESC
+
+10.
+SELECT clie.FirstName AS Nombre_cliente, artists.name AS Nombre_artista FROM customers clie
+JOIN invoices ON clie.CustomerId = invoices.CustomerId
+JOIN invoice_items ON invoices.InvoiceId = invoice_items.InvoiceId
+JOIN tracks ON invoice_items.TrackId = tracks.TrackId
+JOIN albums ON tracks.AlbumId = albums.AlbumId
+JOIN artists ON albums.ArtistId = artists.ArtistId
+ORDER BY Nombre_cliente ASC
+
+11.
+SELECT c.FirstName, c.City, t.name AS Song, g.name AS Genre FROM customers c
+JOIN invoices inv ON c.CustomerId = inv.CustomerId
+JOIN invoice_items inv_i ON inv.InvoiceId = inv_i.InvoiceId
+JOIN tracks t ON inv_i.TrackId = t.TrackId
+JOIN albums a ON t.AlbumId = a.AlbumId
+JOIN artists art ON a.ArtistId = art.ArtistId
+JOIN genres g ON t.GenreId = g.GenreId
+
+12.
+SELECT c.FirstName, c.City, t.name AS Song, g.name AS Genre FROM customers c
+JOIN employees emp ON c.SupportRepId = emp.EmployeeId
+JOIN invoices inv ON c.CustomerId = inv.CustomerId
+JOIN invoice_items inv_i ON inv.InvoiceId = inv_i.InvoiceId
+JOIN tracks t ON inv_i.TrackId = t.TrackId
+JOIN albums a ON t.AlbumId = a.AlbumId
+JOIN artists art ON a.ArtistId = art.ArtistId
+JOIN genres g ON t.GenreId = g.GenreId
+JOIN playlist_track playlst ON t.TrackId = playlst.TrackId
+JOIN playlists plist ON playlst.PlaylistId = plist.PlaylistId
+JOIN media_types mt ON t.MediaTypeId = mt.MediaTypeId
+
+
+
