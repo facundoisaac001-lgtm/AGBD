cd5af16577c99b6589c50e5642fb5e622252e884
facundo <facundoisaac001@gmail.com>
2026-05-26 16:57:44 -0300

    laburo hecho

--- analisis-agbd/importando.py
+++ analisis-agbd/importando.py
@@ -1,4 +1,24 @@
 import pandas as pd
 
-df=pd.read_csv("Vote Ai.csv")
-print ("OKEY! Archivo cargado correctamente")
+df=pd.read_csv('Vote Ai.csv')
+print ('OKEY! Archivo cargado correctamente')
+
+#print(df.head())
+
+#resultado = df['Election_ID'].count()
+#resultado = df['Round'].sum()
+
+
+#print(resultado)
+
+print('---Analizis Avanzado de datos---')
+
+filtro_avanzado = df['Constituency_Name'].str.startswith('Nagpur', na = False)
+df_filtrado = df[filtro_avanzado]
+
+sumo_Rondas = df_filtrado ['Round'].sum()
+
+print('--reporte de rondas--')
+print(f"rondas total analizadas:Rondas {sumo_Rondas} rondas.\n")
+
+filtro_numero = df['Round'] > 8
